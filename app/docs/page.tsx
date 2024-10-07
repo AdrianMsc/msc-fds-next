afee742ad503937151cb5c7edbce249f230b13b3
diegomsc <diego.delacruz@mscdirect.com>
2024-10-07 13:07:55 -0600

    Feat: HomePage v0.1

---
+++ app/docs/page.tsx
@@ -0,0 +1,5 @@
+const DocsPage = () => {
+  return <h1>Docs page</h1>;
+};
+
+export default DocsPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,75 @@
+"use client";
+
+import Link from "next/link";
+import Lottie from "react-lottie";
+import animation from "../assets/animation.json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowRight,
+  faArrowUpRightFromSquare,
+  faBook,
+} from "@fortawesome/free-solid-svg-icons";
+
 export default function Home() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-5xl">MSC FDS</h1>
-      </main>
-    </div>
+    <main className="flex justify-center items-center h-screen">
+      <div className="flex flex-col justify-center px-5 lg:w-[45%]">
+        <h1 className="font-normal text-[4rem] leading-none">
+          Welcome to MSC <br />
+          Design System
+        </h1>
+        <h3 className="font-light text-left">
+          Empowering Innovation Through Unified Design.
+        </h3>
+
+        <div className="py-5 flex flex-wrap place-content-between w-fit">
+          <Link className="msc-btn msc-btn-blue-solid mr-5" href="/docs">
+            Go to Docs
+            <FontAwesomeIcon
+              icon={faArrowRight}
+              className="ml-2"
+              height={16}
+              width={16}
+            />
+          </Link>
+
+          <a
+            className="msc-text-link py-3 mr-4"
+            href="https://adrianmsc.github.io/msc-fuel-design-system-react/?path=/docs/configure-your-project--docs"
+          >
+            Storybook
+            <FontAwesomeIcon
+              icon={faBook}
+              className="mr-5 ml-2 text-[#ff4785]"
+              height={16}
+              width={16}
+            />
+          </a>
+
+          <a
+            className="msc-text-link py-3"
+            href="https://ds-blog-ten.vercel.app/"
+          >
+            Release Notes
+            <FontAwesomeIcon
+              icon={faArrowUpRightFromSquare}
+              className="ml-2"
+              height={16}
+              width={16}
+            />
+          </a>
+        </div>
+      </div>
+      <Lottie options={defaultOptions} height={500} width={500} />
+    </main>
   );
 }
