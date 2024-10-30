1d921e55060865eaecbffcd352d48ad352a80c65
AdrianLona <adrian.lona@mscsdirect.com>
2024-10-30 09:19:48 -0600

    Msc Tabs animation and favicon

--- app/docs/Alert/page.tsx
+++ app/docs/Alert/page.tsx
@@ -1,35 +1,138 @@
-import Codeblock from "@/layout/Codeblock";
-import MscAlert from "@/components/MscAlert";
+import MscTabs from "@/components/MscTabs";
+import React from "react";
 
 const MscAlertPage = () => {
+  const variants = ["Info", "Success", "Warning", "Error"];
+
   return (
-    <>
-      <p className="category-tag">Indicator</p>
-      <h2>Alert</h2>
-      <MscAlert />
-      <Codeblock>
-        {`
-  <div class="msc-alert msc-alert-info w-full">
-    <div class="msc-alert-main-container">
-      <div class="msc-alert-content">
-        <i class="msc-alert-icon msc-alert-info-icon fas fa-circle-exclamation"></i>
-            This is an alert message
+    <div>
+      <h1 className="font-bold text-3xl mb-3">
+        <small className="text-sm text-primary-blue">Alerts</small> <br />
+        Alerts
+      </h1>
+
+      <MscTabs labels={variants} />
+
+      <h2 className="mb-4">CTA Alert</h2>
+      <div className="flex gap-2 items-center bg-white p-5 w-full rounded">
+        <div
+          id="warning-example-id"
+          className="msc-alert msc-alert-info w-full"
+        >
+          <div className="msc-alert-main-container">
+            <div className="msc-alert-content">
+              <i className="msc-alert-icon msc-alert-info-icon fas fa-circle-exclamation"></i>
+              This is an alert message
+            </div>
+            <div className="msc-alert-cta-container pb-0">
+              <button className="msc-btn msc-btn-blue-outline msc-btn-sm">
+                <span>Action 2</span>
+              </button>
+              <button className="msc-btn msc-btn-blue-solid msc-btn-sm">
+                <span>Action 1</span>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="msc-alert-cta-container pb-0">
-        <!-- button component -->
-        <button class="msc-btn msc-btn-blue-outline msc-btn-sm">
-          <span>Action 2</span>
-        </button>
-        <!-- button component -->
-        <button class="msc-btn msc-btn-blue-solid msc-btn-sm">
-          <span>Action 1</span>
-        </button>
+      <h2>Without CTA Alert</h2>
+      <div className="flex gap-2 items-center bg-white p-5 w-full rounded">
+        <div
+          id="warning-example-id"
+          className="msc-alert msc-alert-info w-full"
+        >
+          <div className="msc-alert-main-container">
+            <div className="msc-alert-content">
+              <i className="msc-alert-icon msc-alert-info-icon fas fa-circle-exclamation"></i>
+              This is an error message
+            </div>
+          </div>
+        </div>
+      </div>
+      <h2>Alert Global</h2>
+      <div className="flex gap-2 items-center bg-white p-5 w-full rounded">
+        <div
+          id="warning-example-id"
+          className="msc-alert msc-alert-info w-full"
+        >
+          <div className="msc-alert-main-container">
+            <div className="msc-alert-content">
+              <i className="msc-alert-icon msc-alert-info-icon fas fa-circle-info"></i>
+              <div>
+                <p className="msc-alert-title">Example Title</p>
+                <p className="msc-alert-description">Example Description</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <h2>Without Alert list</h2>
+      <div className="flex gap-2 items-center bg-white p-5 w-full rounded">
+        <div
+          id="warning-example-id"
+          className="msc-alert msc-alert-info w-full"
+        >
+          <div className="msc-alert-main-container">
+            <div className="msc-alert-content">
+              <i className="msc-alert-icon msc-alert-info-icon fas fa-circle-info"></i>
+              <div>
+                <p className="msc-alert-title">Example Title</p>
+                <p className="msc-alert-description">Example Description</p>
+              </div>
+            </div>
+            <div className="msc-alert-cta-container">
+              <button className="msc-btn msc-btn-blue-outline msc-btn-sm">
+                <span>Cancel</span>
+              </button>
+              <button className="msc-btn msc-btn-blue-solid msc-btn-sm">
+                <span>Agree</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <h2>With Alert list</h2>
+      <div className="flex gap-2 items-center bg-white p-5 w-full rounded">
+        <div
+          id="warning-example-id"
+          className="msc-alert msc-alert-info w-full"
+        >
+          <div className="msc-alert-main-container">
+            <div className="msc-alert-content">
+              <i className="msc-alert-icon msc-alert-info-icon fas fa-circle-info"></i>
+              <div>
+                <p className="msc-alert-title">Example Title</p>
+                <p className="msc-alert-description">Example Description</p>
+              </div>
+            </div>
+            <div className="msc-alert-cta-container">
+              <button className="msc-btn msc-btn-blue-outline msc-btn-sm">
+                <span>Cancel</span>
+              </button>
+              <button className="msc-btn msc-btn-blue-solid msc-btn-sm">
+                <span>Agree</span>
+              </button>
+            </div>
+          </div>
+          <div className="msc-alert-list-container group">
+            <button
+              type="button"
+              title="Alerts Information"
+              className="msc-alert-list-toggle"
+            >
+              <span className="msc-alert-list-toggle-show">Show</span>
+              <span className="msc-alert-list-toggle-hide">Hide</span> Alerts
+              <i className="msc-alert-list-toggle-icon fas fa-chevron-down fa-sm"></i>
+            </button>
+            <ul className="msc-alert-list">
+              <li className="msc-alert-list-item">
+                <a>Example list item</a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-  `}
-      </Codeblock>
-    </>
   );
 };
 
---
+++ src/components/MscTabs.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React, { useState } from "react";
+
+export const MscTabs = ({ labels = ["Tab 1", "Tab 2", "Tab 3"] }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleTabClick = (index: number) => {
+    setActiveIndex(index);
+    sessionStorage.setItem("alertType", labels[index]);
+  };
+
+  const getGridColsClass = (numLabels: number) => {
+    switch (numLabels) {
+      case 3:
+        return "grid-cols-3";
+      case 4:
+        return "grid-cols-4";
+      case 5:
+        return "grid-cols-5";
+      case 6:
+        return "grid-cols-6";
+      default:
+        return "grid-cols-3";
+    }
+  };
+  return (
+    <>
+      <ul className={`msc-tabs ${getGridColsClass(labels.length)}`}>
+        {labels.map((label, index) => (
+          <li
+            key={index}
+            className={`col-span-6 md:col-span-1 ${
+              activeIndex === index ? "active" : ""
+            }`}
+            onClick={() => handleTabClick(index)}
+          >
+            <a>{label}</a>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default MscTabs;
